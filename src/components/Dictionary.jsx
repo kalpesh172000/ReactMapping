b38665a8214ac989710c12fa7d769ea03be8f475
kalpesh <kalpesh200071@gmail.com>
2024-06-19 15:43:43 +0530

    created entry component &  mapped emojipedia array to it

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,44 +1,21 @@
+import { emojipedia } from "../emojipedia";
+import Entry from "./Entry";
+
+function addEntry(emojipedia){
+    return (
+        <Entry key={emojipedia.id} emoji={emojipedia.emoji} name={emojipedia.name} meaning={emojipedia.meaning}/>
+    )            
+}
+
 function Dictionary(props) {
     return (
         <dl className="dictionary">
-            <div className="term">
-                <dt>
-                    <span className="emoji" role="img" aria-label="Tense Biceps">
-                        💪
-                    </span>
-                    <span>Tense Biceps</span>
-                </dt>
-                <dd>
-                    “You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing
-                    sports, e.g. at the gym.
-                </dd>
-            </div>
-            <div className="term">
-                <dt>
-                    <span className="emoji" role="img" aria-label="Tense Biceps">
-                        🙏
-                    </span>
-                    <span>Person With Folded Hands</span>
-                </dt>
-                <dd>
-                    Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for
-                    enlightenment. Is also used as a “high five” or to say thank you.
-                </dd>
-            </div>
-            <div className="term">
-                <dt>
-                    <span className="emoji" role="img" aria-label="Tense Biceps">
-                        🤣
-                    </span>
-                    <span>Rolling On The Floor, Laughing</span>
-                </dt>
-                <dd>
-                    This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The
-                    emoji version of “rofl“. Stands for „rolling on the floor, laughing“.
-                </dd>
-            </div>
+            {
+                emojipedia.map(addEntry)
+            }
         </dl>
     );
 }
 
-export default Dictionary;
+export { Dictionary };
+
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
 
 // If you're running this locally in VS Code use the commands:
 // npm install
